0555a97cf16836331e778efe52c47e97f0074271
Iouri Belikov <iouri.belikov@cern.ch>
2023-02-09 16:16:47 +0100

    More histograms. Selection of tracks' IPs.

--- Tutorials/src/taskv0s.cxx
+++ Tutorials/src/taskv0s.cxx
@@ -51,11 +51,6 @@ double Det(double a00, double a01, double a02,
 }
 
 struct taskv0s {
-  Configurable<float> cfgBz{"Bz", 5., "Bz component of the solenoid magnetic field (kG)"};
-  Configurable<float> cfgDcaDaugh = {"dcaDaugh", 0.05, "Max. allowed DCA between daughters"};
-  Configurable<float> cfgRmin = {"rMin", 0.3, "Min. allowed radius for a V0 decay"};
-  Configurable<float> cfgRmax = {"rMax", 100., "Max. allowed radius for a V0 decay"};
-
   float piMass = 0.1396;
   float pMass = 0.9383;
   float lambdaMass = 1.1157;
@@ -64,12 +59,23 @@ struct taskv0s {
   float mChi2;
   float mR;
   float mD;
+  float mX, mY, mZ;
+
+  float mPxp, mPyp, mPzp, mPxn, mPyn, mPzn;
+  float mDp, mZp, mDn, mZn;
 
   float mK0sMass;
   float mLambdaMass;
   float mLambdaBarMass;
 
-  OutputObj<TH1F> hVtxSel{
+  o2::vertexing::DCAFitter2 mFitter;
+
+  Configurable<float> cfgBz{"Bz", 5., "Bz component of the solenoid magnetic field (kG)"};
+  Configurable<float> cfgDcaDaugh = {"dcaDaugh", 0.05, "Max. allowed DCA between daughters"};
+  Configurable<float> cfgRmin = {"rMin", 0.5, "Min. allowed radius for a V0 decay"};
+  Configurable<float> cfgRmax = {"rMax", 70., "Max. allowed radius for a V0 decay"};
+
+  OutputObj<TH1F> hVtx{
     TH1F("hVtx", "Primary vertex position after selection; Z (cm)", 100, -20., 20.)};
 
   OutputObj<TH2F> hdEdx{
@@ -77,15 +83,36 @@ struct taskv0s {
   OutputObj<TH2F> hdEdxSel{
     TH2F("hdEdxSel", "TPC after selection; Momentum (GeV); dE/dx", 200, 0., 4., 200, 0., 1000)};
 
+  OutputObj<TH1F> hChi2{
+    TH1F("hChi2", "Chi2 of the V0 vertex; Chi2", 100, 0., 33.)};
+  OutputObj<TH2F> hDvsR{
+    TH2F("hDvsR", "D vs R; R (cm); D (cm)", 100, 0., 100., 100, 0., 0.5)};
+
   OutputObj<TH1F> hK0sMass{
     TH1F("hK0sMass", "K0s mass; Mass (GeV)", 100, k0sMass - 0.2, k0sMass + 0.2)};
-  OutputObj<TH1F> hK0sMassSel{
-    TH1F("hK0sMassSel", "K0s mass after selection; Mass (GeV)", 100, k0sMass - 0.2, k0sMass + 0.2)};
-
   OutputObj<TH1F> hLambdaMass{
-    TH1F("hLambdaMass", "Lambda+LambdaBar mass; Mass (GeV)", 120, lambdaMass - 0.2, lambdaMass + 0.2)};
-  OutputObj<TH1F> hLambdaMassSel{
-    TH1F("hLambdaMassSel", "Lambda+LambdaBar mass after selection; Mass (GeV)", 120, lambdaMass - 0.2, lambdaMass + 0.2)};
+    TH1F("hLambdaMass", "Lambda mass; Mass (GeV)", 120, lambdaMass - 0.2, lambdaMass + 0.2)};
+  OutputObj<TH1F> hLambdaBarMass{
+    TH1F("hLambdaBarMass", "LambdaBar mass; Mass (GeV)", 120, lambdaMass - 0.2, lambdaMass + 0.2)};
+
+  OutputObj<TH2F> hArm{
+    TH2F("hArm", "Armenteros plot;#alpha;q_{T} (GeV/c)", 200, -1., 1., 200, 0, 0.25)};
+  OutputObj<TH2F> hRb2{
+    TH2F("hRb2", "Interaction vertices;X (cm);Y (cm)", 200, -3, 3, 200, -3, 3)};
+
+  void init(o2::framework::InitContext& ic)
+  {
+    hDvsR->SetBins(100, 0., 1.5 * cfgRmax, 100, 0., 1.5 * cfgDcaDaugh);
+
+    mFitter.setBz(cfgBz);
+    mFitter.setPropagateToPCA(true);
+    mFitter.setMaxR(cfgRmax);
+    mFitter.setMaxDZIni(10 * cfgDcaDaugh);
+    mFitter.setMaxDXYIni(10 * cfgDcaDaugh);
+    mFitter.setMinParamChange(1e-3);
+    mFitter.setMinRelChi2Change(0.9);
+    mFitter.setMaxChi2(30);
+  }
 
   void v0Mass(float pxp, float pyp, float pzp, float pxn, float pyn, float pzn)
   {
@@ -112,6 +139,40 @@ struct taskv0s {
     mLambdaBarMass = sqrt(e * e - p2);
   }
 
+  void impactParams(myTrack const& track, float x, float y, float z, float bz, float ip[2])
+  {
+    //------------------------------------------------------------------
+    // This function calculates the transverse and longitudinal impact parameters
+    // with respect to a point with global coordinates (x,y,0)
+    // in the magnetic field "bz" (kG)
+    //------------------------------------------------------------------
+    auto f1 = track.snp();
+    auto r1 = sqrt((1. - f1) * (1. + f1));
+    auto xt = track.x(), yt = track.y();
+    auto sn = sin(track.alpha()), cs = cos(track.alpha());
+    float a = x * cs + y * sn;
+    y = -x * sn + y * cs;
+    x = a;
+    xt -= x;
+    yt -= y;
+
+    float rp4 = track.signed1Pt() * bz * o2::constants::math::B2C;
+    if ((abs(bz) < 1e-33) || (abs(rp4) < 1e-33)) {
+      ip[0] = -(xt * f1 - yt * r1);
+      ip[1] = track.z() + (ip[0] * f1 - xt) / r1 * track.tgl() - z;
+      return;
+    }
+
+    sn = rp4 * xt - f1;
+    cs = rp4 * yt + r1;
+    a = 2 * (xt * f1 - yt * r1) - rp4 * (xt * xt + yt * yt);
+    auto rr = sqrt(sn * sn + cs * cs);
+    ip[0] = -a / (1 + rr);
+    auto f2 = -sn / rr;
+    auto r2 = sqrt((1. - f2) * (1. + f2));
+    ip[1] = track.z() + track.tgl() / rp4 * asin(f2 * r1 - f1 * r2) - z;
+  }
+
   // Collision selector
   bool isCollisionAccepted(aod::Collision const& collision)
   {
@@ -123,20 +184,34 @@ struct taskv0s {
 
   // Track-quality selector
   template <typename TrackInstance>
-  bool isTrackAccepted(TrackInstance const& track)
+  bool isTrackAccepted(TrackInstance const& track, float ip[2])
   {
-    // if (track.itsNCls() != 7)
-    //   return false;
+    if (track.itsNCls() < 6)
+      return false;
     if (abs(track.eta()) > 0.9)
       return false;
+
+    impactParams(track, 0, 0, 0, cfgBz, ip);
+    if (abs(ip[0]) < 0.02)
+      return false;
+
     return true;
   }
 
   // V0 selector
-  bool isV0Accepted(myV0 const& v0)
+  bool isV0Accepted(myTrack const& pos, myTrack const& neg)
   {
-    const auto& pos = v0.posTrack_as<myTracks>();
-    const auto& neg = v0.negTrack_as<myTracks>();
+
+    float ip[2];
+    if (!isTrackAccepted(pos, ip))
+      return false;
+    mDp = ip[0];
+    mZp = ip[1];
+
+    if (!isTrackAccepted(neg, ip))
+      return false;
+    mDn = ip[0];
+    mZn = ip[1];
 
     auto ptrack = getTrackParCov(pos);
     auto ntrack = getTrackParCov(neg);
@@ -169,6 +244,9 @@ struct taskv0s {
       return false;
     if (r2 > cfgRmax * cfgRmax)
       return false;
+    mX = x;
+    mY = y;
+    mZ = z;
 
     const auto& t0 = mFitter.getTrack(0, ibest);
     const auto& t1 = mFitter.getTrack(1, ibest);
@@ -190,20 +268,31 @@ struct taskv0s {
     std::array<float, 3> p1;
     t1.getPxPyPzGlo(p1);
 
+    mPxp = p0[0];
+    mPyp = p0[1];
+    mPzp = p0[2];
+    mPxn = p1[0];
+    mPyn = p1[1];
+    mPzn = p1[2];
+
     v0Mass(p0[0], p0[1], p0[2], p1[0], p1[1], p1[2]);
 
     return true;
   }
 
   // My V0 selector
-  bool isV0AcceptedMy(myV0 const& v0)
+  bool isV0AcceptedMy(myTrack const& p1, myTrack const& p2)
   {
-    auto const& p1 = v0.posTrack_as<myTracks>();
-    if (!isTrackAccepted(p1))
+    float ip[2];
+    if (!isTrackAccepted(p1, ip))
       return false;
-    auto const& p2 = v0.negTrack_as<myTracks>();
-    if (!isTrackAccepted(p2))
+    mDp = ip[0];
+    mZp = ip[1];
+
+    if (!isTrackAccepted(p2, ip))
       return false;
+    mDn = ip[0];
+    mZn = ip[1];
 
     // Topological selections...
     auto px1 = p1.px();
@@ -235,7 +324,8 @@ struct taskv0s {
                 Det(px1, py1, pz1, px2, py2, pz2, ax, ay, az);
 
     x1 += px1 * t1;
-    y1 += py1 * t1; // z1 += pz1*t1;
+    y1 += py1 * t1;
+    z1 += pz1 * t1;
 
     float r2 = x1 * x1 + y1 * y1;
     if (r2 > cfgRmax * cfgRmax)
@@ -246,6 +336,16 @@ struct taskv0s {
     mChi2 = 0.;
     mR = sqrt(r2);
     mD = dca;
+    mX = x1;
+    mY = y1;
+    mZ = z1;
+
+    mPxp = px1;
+    mPyp = py1;
+    mPzp = pz1;
+    mPxn = px2;
+    mPyn = py2;
+    mPzn = pz2;
 
     v0Mass(px1, py1, pz1, px2, py2, pz2);
 
@@ -289,20 +389,6 @@ struct taskv0s {
     return true;
   }
 
-  o2::vertexing::DCAFitter2 mFitter;
-
-  void init(o2::framework::InitContext& ic)
-  {
-    mFitter.setBz(cfgBz);
-    mFitter.setPropagateToPCA(true);
-    mFitter.setMaxR(30);
-    mFitter.setMaxDZIni(0.1);
-    mFitter.setMaxDXYIni(0.1);
-    mFitter.setMinParamChange(1e-3);
-    mFitter.setMinRelChi2Change(0.9);
-    mFitter.setMaxChi2(10);
-  }
-
   void process(aod::Collision const& collision, myTracks const& tracks, myV0s const& v0s)
   {
     static int ncol = 0;
@@ -313,7 +399,7 @@ struct taskv0s {
       return;
 
     // Basic collision counter...
-    hVtxSel->Fill(collision.posZ());
+    hVtx->Fill(collision.posZ());
 
     for (auto& track : tracks) {
       auto mom = track.tpcInnerParam();
@@ -324,34 +410,47 @@ struct taskv0s {
     // LOG(info) << "V0s: " << v0s.size() << " tracks: " << tracks.size();
 
     for (auto& v0 : v0s) {
-      const auto& pos = v0.posTrack_as<myTracks>();
-      const auto& neg = v0.negTrack_as<myTracks>();
+      auto const& pos = v0.posTrack_as<myTracks>();
+      auto const& neg = v0.negTrack_as<myTracks>();
 
       // No selections yet...
-      v0Mass(pos.px(), pos.py(), pos.pz(), neg.px(), neg.py(), neg.pz());
-      hK0sMass->Fill(mK0sMass);
-      hLambdaMass->Fill(mLambdaMass);
-      hLambdaMass->Fill(mLambdaBarMass);
+      // v0Mass(pos.px(), pos.py(), pos.pz(), neg.px(), neg.py(), neg.pz());
 
       /*
-      if (!isV0AcceptedMy(v0))  // Masses are re-calculated here
+      if (!isV0AcceptedMy(pos, neg))  // Masses are re-calculated here
         continue;
       */
-      if (!isV0Accepted(v0)) // Masses are re-calculated here
+      if (!isV0Accepted(pos, neg)) // Masses are re-calculated here
         continue;
+
+      // Armenteros
+      auto px = mPxp + mPxn;
+      auto py = mPyp + mPyn;
+      auto pz = mPzp + mPzn;
+      auto p = sqrt(px * px + py * py + pz * pz);
+      auto pLp = (mPxp * px + mPyp * py + mPzp * pz) / p;
+      auto pLn = (mPxn * px + mPyn * py + mPzn * pz) / p;
+      auto alpha = (pLp - pLn) / (pLp + pLn);
+      auto qt = sqrt(mPxp * mPxp + mPyp * mPyp + mPzp * mPzp - pLp * pLp);
+      hArm->Fill(alpha, qt);
+
+      hRb2->Fill(mX, mY);
 
       // PID
       if (isK0sLikeV0(pos, neg)) {
         hdEdxSel->Fill(pos.tpcInnerParam(), pos.tpcSignal());
         hdEdxSel->Fill(neg.tpcInnerParam(), neg.tpcSignal());
-        hK0sMassSel->Fill(mK0sMass);
+        hK0sMass->Fill(mK0sMass);
       }
       if (isLambdaLikeV0(pos, neg)) {
-        hLambdaMassSel->Fill(mLambdaMass);
+        hLambdaMass->Fill(mLambdaMass);
       }
       if (isLambdaLikeV0(neg, pos)) {
-        hLambdaMassSel->Fill(mLambdaBarMass);
+        hLambdaBarMass->Fill(mLambdaBarMass);
       }
+
+      hChi2->Fill(mChi2);
+      hDvsR->Fill(mR, mD);
     }
   }
 };
